db364a69c7e1d055acc943b2ec3bc5d5e76caa58
brysonwaisi <brysonnyamwange@gmail.com>
2024-02-26 21:30:43 +0300

    memoization and tabulation

--- memoization/canSum.js
+++ memoization/canSum.js
@@ -25,24 +25,23 @@
 // console.log(canSum(8, [2, 3, 5]));
 // console.log(canSum(300, [7, 14]));
 
+// **Memoized**
 
-            // **Memoized**
-
-function  canSum (targetSum, numbers, memo={}) {
-    if (targetSum in memo) return memo[targetSum]
+function canSum(targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return true;
   if (targetSum < 0) return false;
 
   for (let num of numbers) {
     const remainder = targetSum - num;
     if (canSum(remainder, numbers, memo) === true) {
-        memo[targetSum] = true
+      memo[targetSum] = true;
       return true;
     }
   }
-  memo[targetSum] = false
+  memo[targetSum] = false;
   return false;
-};
+}
 
 console.log(canSum(7, [2, 3]));
 console.log(canSum(7, [5, 3, 4, 7]));
